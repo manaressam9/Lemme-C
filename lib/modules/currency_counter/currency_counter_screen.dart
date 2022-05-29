dd584c57fff7c648a78ab62206da51b09f7878fe
Mu7ammadGamal <mg1297881@gmail.com>
2022-05-29 17:47:02 +0200

    Adding takePic() instead of startImgStream() into textRec module

--- lib/modules/currency_counter/currency_counter_screen.dart
+++ lib/modules/currency_counter/currency_counter_screen.dart
@@ -96,16 +96,20 @@ class _CurrencyCounterState extends State<CurrencyCounter> {
 
   /// Callback to get inference results from [CameraView]
   void resultsCallback(List<Recognition>? results) {
-    setState(() {
-      this.results = results;
-    });
+    if(mounted){
+      setState(() {
+        this.results = results;
+      });
+    }
   }
 
   /// Callback to get inference stats from [CameraView]
   void statsCallback(Stats stats) {
-    setState(() {
-      this.stats = stats;
-    });
+    if(mounted){
+      setState(() {
+        this.stats = stats;
+      });
+    }
   }
 
   static const BOTTOM_SHEET_RADIUS = Radius.circular(24.0);
